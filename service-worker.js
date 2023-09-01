92085503e23cf4eda8d53724003b18f433e82be8
Rodrigo <roh.amelo@gmail.com>
2023-09-01 09:28:33 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.201b09f1dac2735f32d777583f9de104.js"
+  "/precache-manifest.2705777012620f5275e2feae0d0c372c.js"
 );
 
 workbox.clientsClaim();
