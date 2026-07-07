3127ec478ea50e8d9f5d3643c2628d573bd12dab
Rodrigo <roh.amelo@gmail.com>
2026-07-07 16:38:01 -0300

    Updates

--- precache-manifest.7e8182b7406fadb70a49534bb5206943.js
+++ precache-manifest.7e8182b7406fadb70a49534bb5206943.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "bef80efa04554edca007",
+    "revision": "3c599abbe9a3d7a5629d",
     "url": "/static/css/main.002385a4.chunk.css"
   },
   {
-    "revision": "bef80efa04554edca007",
-    "url": "/static/js/main.af4f5c90.chunk.js"
+    "revision": "3c599abbe9a3d7a5629d",
+    "url": "/static/js/main.47a80ca3.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "19bdfece17d3947c7c28",
-    "url": "/static/js/2.9ef3e7c0.chunk.js"
+    "revision": "e165569b9e3e00495eb8",
+    "url": "/static/js/2.4a0c6039.chunk.js"
   },
   {
     "revision": "216143ba4d4ee59ec9713c7c0c9bc3e9",
     "url": "/static/media/InterglacticTransmissing.216143ba.nes"
   },
   {
-    "revision": "ca9329f1b85689feba0ca1ca13c46b89",
+    "revision": "3c610f3c052cdf9d35c41a6244254ea8",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e2d1455416da62b45603160b9e58a070.js"
+  "/precache-manifest.7e8182b7406fadb70a49534bb5206943.js"
 );
 
 workbox.clientsClaim();
